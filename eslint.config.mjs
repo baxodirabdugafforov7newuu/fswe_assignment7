cebbaa2ad3e234b25baf29a549ca518dbe8a73c7
baxodirabdugafforov7newuu <b.abdugafforov@newuu.uz>
2025-11-16 16:20:01 +0500

    fixed errors in transcriptManager.ts, js config files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,24 @@
-// @ts-check
+/* eslint-env node */
 
-import eslint from '@eslint/js';
-import { defineConfig } from 'eslint/config';
-import tseslint from 'typescript-eslint';
-
-export default defineConfig(
-  eslint.configs.recommended,
-  tseslint.configs.recommended,
-);
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    // your custom rules if needed
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+  },
+};
